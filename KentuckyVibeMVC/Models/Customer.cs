d95978800770b97938dc24ccbe728e4e9a1b3f3e
Jeff Martin <Jeffm3777@gmail.com>
2022-07-28 20:23:29 -0400

    Added Models

---
+++ KentuckyVibeMVC/Models/Customer.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace KentuckyVibeMVC.Models
+{
+    public class Customer
+    {
+        public int CustomerId { get; set; }
+        public string CustomerName { get; set; }
+        public string Email { get; set; }
+        public int FavoriteDrinkId { get; set; }
+        [ForeignKey("FavoriteDrinkId")]
+        public virtual Drink FavoriteDrink { get; set; }
+    }
+}
---
+++ KentuckyVibeMVC/Models/FavoriteDrinks.cs
@@ -0,0 +1,15 @@
+﻿namespace KentuckyVibeMVC.Models
+{
+    public class FavoriteDrinks
+    {
+        // Drink List
+        public int FavoriteId { get; set; } 
+        public string FavoriteName { get; set; }
+        public int Drinks { get; set; }
+        public int Number { get => number; set => number = value; }
+
+        private int number;
+
+
+    }
+}
