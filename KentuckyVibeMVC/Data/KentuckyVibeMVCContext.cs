23e55f85abefedf887faf7c026199c80ee533904
Jeff Martin <Jeffm3777@gmail.com>
2022-07-28 20:25:13 -0400

    Commiting the rest

---
+++ KentuckyVibeMVC/Data/KentuckyVibeMVCContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using KentuckyVibeMVC.Models;
+
+namespace KentuckyVibeMVC.Data
+{
+    public class KentuckyVibeMVCContext : DbContext
+    {
+        public KentuckyVibeMVCContext (DbContextOptions<KentuckyVibeMVCContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<KentuckyVibeMVC.Models.Customer> Customers { get; set; }
+        public DbSet<KentuckyVibeMVC.Models.Drink> Drinks { get; set; }
+    }
+}
---
+++ KentuckyVibeMVC/Migrations/20220725221543_Drinks.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace KentuckyVibeMVC.Migrations
+{
+    public partial class Drinks : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Drinks",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Price = table.Column<double>(type: "float", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Drinks", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Customers",
+                columns: table => new
+                {
+                    CustomerId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CustomerName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    FavoriteDrinkId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Customers", x => x.CustomerId);
+                    table.ForeignKey(
+                        name: "FK_Customers_Drinks_FavoriteDrinkId",
+                        column: x => x.FavoriteDrinkId,
+                        principalTable: "Drinks",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Customers_FavoriteDrinkId",
+                table: "Customers",
+                column: "FavoriteDrinkId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Customers");
+
+            migrationBuilder.DropTable(
+                name: "Drinks");
+        }
+    }
+}
